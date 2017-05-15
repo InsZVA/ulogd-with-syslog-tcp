4011833e10de65f618ff63b270bd30e8db89c56e
Zile Li <inszva@126.com>
2017-05-15 16:03:42 +0800

    fix bug

--- util/speedup.c
+++ util/speedup.c
@@ -10,16 +10,19 @@ void appendStr(char** pDstStr, char* srcStr) {
 void appendUint(char** pDstStr, unsigned int n) {
 	int num = n;
 	int digits = 0;
+	char* end = 0;
 	while (num) {
 		num /= 10;
 		(*pDstStr)++;
 	}
+	end = *pDstStr;
+	*end = 0;
 	while (n) {
         int digit = n % 10;
         *(--(*pDstStr)) = "0123456789"[digit];
         n >>= 10;
     }
-	**pDstStr = 0;
+	*pDstStr = end;
 }
 
 void appendHEX8(char** pDstStr, int n) {
@@ -34,14 +37,12 @@ void appendHex8(char** pDstStr, int n) {
 	int i = 0;
 	for (; i < 8; i++)
 		*((*pDstStr)++) = "0123456789abcdef"[(n >> ((7 - i) << 2)) & 0xf];
-	--(*pDstStr);
 	**pDstStr = 0;
 }
 
 void appendHEX2(char** pDstStr, int n) {
 	*((*pDstStr)++) = "0123456789ABCDEF"[(n >> 4) & 0xf];
 	*((*pDstStr)++) = "0123456789ABCDEF"[n & 0xf];
-	--(*pDstStr);
 	**pDstStr = 0;
 }
 
@@ -49,22 +50,24 @@ void appendHEX2(char** pDstStr, int n) {
 void appendHex2(char** pDstStr, int n) {
 	*((*pDstStr)++) = "0123456789abcdef"[n >> 4];
 	*((*pDstStr)++) = "0123456789abcdef"[n & 0xf];
-	--(*pDstStr);
 	**pDstStr = 0;
 }
 
 void appendHex(char** pDstStr, int n) {
 	int num = n;
 	int digits = 0;
+	char *end = 0;
 	while (num) {
 		num >>= 4;
 		(*pDstStr)++;
 	}
+	end = *pDstStr;
 	while (n) {
         int digit = n & 0xf;
         *(--(*pDstStr)) = "0123456789abcdef"[digit];
         n >>= 4;
     }
+	*pDstStr = end;
     **pDstStr = 0;
 }
 
